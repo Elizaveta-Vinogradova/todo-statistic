e024ba80bed75ea9af406851c0d1f1f78b7bafa0
Vladislav Ershov <vlad0303@outlook.com>
2026-03-03 18:29:00 +0300

    2

--- index.js
+++ index.js
@@ -24,25 +24,56 @@ function skipWhile(str, predicate){
     return result.join("");
 }
 
+function countOf(str, char) {
+    return str.split("").filter(x => x === char).length;
+}
+
 function isWhitespace(c) {
     return c === ' ' || c === 't';
 }
 
+function getTODO(){
+    const lines = getFiles()
+        .flatMap((line) => line.split('//'))
+        .flatMap((line) => line.split('\r\n'));
+    let noWhitespaces = lines.map(t => skipWhile(t, isWhitespace));
+    let filtered = noWhitespaces.filter(t => t.startsWith('TODO '));
+    return filtered;
+}
+
+function getMetadata(todoStr) {
+    const parts = todoStr.split(';');
+    if(parts.length === 1) {
+        return {
+            user : "",
+            date : "",
+            message : parts[0].split(' ')[1],
+            original: todoStr
+        };
+    }
+    const user = parts[0].split(' ')[1];
+    const date = Date.parse(parts[1]);
+    const message = parts[2];
+    return {
+        user : user,
+        date : date,
+        message : message,
+        original: todoStr
+    }
+}
+
 function processCommand(command) {
+    const list = getTODO();
     switch (command) {
         case 'exit':
             process.exit(0);
             break;
         case 'show':
-            const lines = getFiles().flatMap((line) => line.split('//')).flatMap((line) => line.split('\r\n'));
-            let noWhitespaces = lines.map(t => skipWhile(t, isWhitespace));
-            console.log(noWhitespaces);
-            let filtered = noWhitespaces.filter(t => t.startsWith('TODO '));
-            console.log('ANSWEER');
-            console.log(filtered);
+            console.log(list);
             break;
         case 'important':
-            console.log(getFiles());
+            const result = list.filter(t => t.includes('!'));
+            console.log(result);
             break;
         default:
             const splitLine = command.split(' ');
@@ -50,14 +81,29 @@ function processCommand(command) {
             const value = splitLine[1];
             switch (commandNew) {
                 case 'user':
+                    const user = value;
+                    const users = list.filter(t => t.startsWith(`TODO ${user}`));
+                    console.log(users);
                     break;
                 case 'sort':
                     switch(value){
                         case 'importance':
+                            const result = list.sort((a, b) => - countOf(a, '!') + countOf(b, '!'))
+                            console.log(result);
                             break;
                         case 'user':
+                            const metadata = list
+                                .map(getMetadata)
+                                .sort((a, b) => a.user > b.user)
+                                .map(m => m.original);
+                            console.log(metadata);
                             break;
                         case 'date':
+                            const metadata2 = list
+                                .map(getMetadata)
+                                .sort((a, b) => a.date > b.date)
+                                .map(m => m.date);
+                            console.log(metadata2);
                             break;
                         default:
                             console.log('invalid argument');
